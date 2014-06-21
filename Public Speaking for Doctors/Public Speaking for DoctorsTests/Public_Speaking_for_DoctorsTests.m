cbf8af047f2e341201c4b7aeae698a0bb81b12a3
AbeeraKamran <abeera.k@gmail.com>
2014-06-21 17:30:05 +0100

    overlaying blurb

---
+++ Public Speaking for Doctors/Public Speaking for DoctorsTests/Public_Speaking_for_DoctorsTests.m
@@ -0,0 +1,34 @@
+//
+//  Public_Speaking_for_DoctorsTests.m
+//  Public Speaking for DoctorsTests
+//
+//  Created by Abeera Kamran on 12/06/2014.
+//  Copyright (c) 2014 ___MUJALLED___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Public_Speaking_for_DoctorsTests : XCTestCase
+
+@end
+
+@implementation Public_Speaking_for_DoctorsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
