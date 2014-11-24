f41b3b203f5b9ec7f342cc8d6f8276e4f3023556
AbeeraKamran <abeera.k@gmail.com>
2014-11-24 11:17:59 +0000

    developing contact page
    building form flip

--- Public Speaking for Doctors-Web files/js/main.js
+++ Public Speaking for Doctors-Web files/js/main.js
@@ -1,1 +1,20 @@
-$('.left-off-canvas-toggle').on('click', function(){});
+// $( document ).ready(function() {
+
+// 	var trigger = $(".menu-icon");
+// 	var nav = $("nav");
+
+// 	// hover on trigger - display half menu
+// 	trigger.mouseover(function() {
+// 		$("#nav").attr("style","overflow:visible");
+
+// 	});
+
+	
+
+// });
+$(document).ready(function(){
+  $('.turn').click(function(){
+    $('.cont-turn').toggleClass('flipped');
+    return false;
+  });
+});
