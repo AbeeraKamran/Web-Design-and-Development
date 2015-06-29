363c6d2b54ee8f801d110d875eb1a3bac92b91ae
AbeeraKamran <abeera.k@gmail.com>
2015-06-29 16:41:53 +0100

    Built first text page

--- OOMK-Internet Project/js/main.js
+++ OOMK-Internet Project/js/main.js
@@ -1,1 +1,15 @@
+$(function() {
+//     $( "#dialog-message" ).dialog({
+//       modal: true,
+//       buttons: {
+//         Ok: function() {
+//           $( this ).dialog( "close" );
+//         }
+//       }
+//     });
+//   
+// $(document).foundation();
+// $(document).ready(function(){$('#myModal').foundation('reveal', 'open')});
+Browser.msgBox('hello world');
+});
 
