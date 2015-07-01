1f661c6cb54f15ee0a7d0efb77d0166f3dfa59be
AbeeraKamran <abeera.k@gmail.com>
2015-07-01 11:25:50 +0100

    Building second section of text

--- OOMK-Internet Project/js/main.js
+++ OOMK-Internet Project/js/main.js
@@ -1,15 +1,18 @@
 $(function() {
-//     $( "#dialog-message" ).dialog({
-//       modal: true,
-//       buttons: {
-//         Ok: function() {
-//           $( this ).dialog( "close" );
-//         }
-//       }
-//     });
-//   
-// $(document).foundation();
-// $(document).ready(function(){$('#myModal').foundation('reveal', 'open')});
-Browser.msgBox('hello world');
+
+
+    // function pageLoad()
+
+    // {
+    //     alert('Respect the delicate ecology of your delusions');
+    // }
+
+    // {
+    //     alert('This is a retreat, a vacuum, it is a virtue is that it lacks everything; deep-freeze for feelings. You can be numb and safe here,that is what you came for.');
+    // }
+
+    // pageLoad(); 
+
+
 });
 
