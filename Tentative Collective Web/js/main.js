1538827d3cf5d8790c69991857cccb6d39b87651
AbeeraKamran <abeera.k@gmail.com>
2015-07-11 00:40:58 +0100

    started building mkmc

--- Tentative Collective Web/js/main.js
+++ Tentative Collective Web/js/main.js
@@ -1,88 +1,82 @@
-
-$(document).ready(function(){
-	$(".menu-toggle a").click(function(){ 
-		$(".menu").slideToggle(700);
-	});
-});
 /*! Zak Group Boilerplate ~ http://zakgroup.co.uk/ ~ Rupert Dunk */
 
 (function($) {
-	$.fn.closestToOffset = function(offset) {
-	    var el = null, elOffset, x = offset.left, y = offset.top, distance, dx, dy, minDistance;
-	    this.each(function() {
-	        elOffset = $(this).offset();
+	// $.fn.closestToOffset = function(offset) {
+	//     var el = null, elOffset, x = offset.left, y = offset.top, distance, dx, dy, minDistance;
+	//     this.each(function() {
+	//         elOffset = $(this).offset();
 
-	        if (
-	        (x >= elOffset.left)  && (x <= elOffset.right) &&
-	        (y >= elOffset.top)   && (y <= elOffset.bottom)
-	        ) {
-	            el = $(this);
-	            return false;
-	        }
+	//         if (
+	//         (x >= elOffset.left)  && (x <= elOffset.right) &&
+	//         (y >= elOffset.top)   && (y <= elOffset.bottom)
+	//         ) {
+	//             el = $(this);
+	//             return false;
+	//         }
 
-	        var offsets = [[elOffset.left, elOffset.top], [elOffset.right, elOffset.top], [elOffset.left, elOffset.bottom], [elOffset.right, elOffset.bottom]];
-	        for (off in offsets) {
-	            dx = offsets[off][0] - x;
-	            dy = offsets[off][1] - y;
-	            distance = Math.sqrt((dx*dx) + (dy*dy));
-	            if (minDistance === undefined || distance < minDistance) {
-	                minDistance = distance;
-	                el = $(this);
-	            }
-	        }
-	    });
-	    return el;
-	}
+	//         var offsets = [[elOffset.left, elOffset.top], [elOffset.right, elOffset.top], [elOffset.left, elOffset.bottom], [elOffset.right, elOffset.bottom]];
+	//         for (off in offsets) {
+	//             dx = offsets[off][0] - x;
+	//             dy = offsets[off][1] - y;
+	//             distance = Math.sqrt((dx*dx) + (dy*dy));
+	//             if (minDistance === undefined || distance < minDistance) {
+	//                 minDistance = distance;
+	//                 el = $(this);
+	//             }
+	//         }
+	//     });
+	//     return el;
+	// }
 
-	function initializeMaps() {
-		var mapStyle = [{"elementType": "geometry", "stylers": [{ "visibility": "simplified" }, { "saturation": -100 }, { "gamma": 1.6 } ] },{"elementType": "labels", "stylers": [{"saturation": -100}]},{"featureType": "water", "stylers": [{ "gamma": 3}, { "hue": "#ffffff" } ] }];var mapStyle = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"off"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{ "visibility": "simplified" },{"gamma": 1.6},{"saturation":-97}]}];
-		$('.map').each(function(){
-			var data = $(this).data('map');
-			if (data){
-				var InitLatlng = new google.maps.LatLng(data[0].marker.lat, data[0].marker.lng);
-				var mapOptions = {center: InitLatlng, zoomControl: 1, zoom: 15 };
-				var map = new google.maps.Map(document.getElementById($(this).attr('id')), mapOptions);
-				map.markers = [];
-				for (var key in data) {
-					if (data.hasOwnProperty(key)) {
-						var obj = data[key];
-						var Latlng = new google.maps.LatLng(obj.marker.lat, obj.marker.lng);
-						var marker = new google.maps.Marker({ title: obj.title, animation: google.maps.Animation.DROP, position: Latlng,map: map, icon: 'img/marker.png' });
-						map.markers.push(marker);
-						var contentString = '<div class="map-marker"><h3>'+obj.title+'</h3><p>'+obj.marker.address+'</p></div>';
-						var infowindow = new google.maps.InfoWindow({ content: obj.title });
-						google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
-							return function() {
-								infowindow.setContent(content);
-								infowindow.open(map,marker);
-							};
-						})(marker,contentString,infowindow));
-						google.maps.event.addListener(map, 'click', (function(marker,content,infowindow) {
-							return function() {
-							infowindow.close(map,marker);
-							};
-						})(marker,contentString,infowindow));
-						infowindow.setContent(contentString);
-						infowindow.open(map,marker);
-					}
-				}
-				map.set('styles', mapStyle);
-				map.setOptions({'scrollwheel': false});
-				var bounds = new google.maps.LatLngBounds();
-				$.each(map.markers, function(i, marker){
-					var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
-					bounds.extend(latlng);
-				});
-				if( map.markers.length == 1 ) {
-					map.setCenter( bounds.getCenter() );
-				} else {
-					map.fitBounds(bounds);
-				}
-			} else {
-				$(this).remove();
-			}
-		});
-	}
+	// function initializeMaps() {
+	// 	var mapStyle = [{"elementType": "geometry", "stylers": [{ "visibility": "simplified" }, { "saturation": -100 }, { "gamma": 1.6 } ] },{"elementType": "labels", "stylers": [{"saturation": -100}]},{"featureType": "water", "stylers": [{ "gamma": 3}, { "hue": "#ffffff" } ] }];var mapStyle = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"off"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{ "visibility": "simplified" },{"gamma": 1.6},{"saturation":-97}]}];
+	// 	$('.map').each(function(){
+	// 		var data = $(this).data('map');
+	// 		if (data){
+	// 			var InitLatlng = new google.maps.LatLng(data[0].marker.lat, data[0].marker.lng);
+	// 			var mapOptions = {center: InitLatlng, zoomControl: 1, zoom: 15 };
+	// 			var map = new google.maps.Map(document.getElementById($(this).attr('id')), mapOptions);
+	// 			map.markers = [];
+	// 			for (var key in data) {
+	// 				if (data.hasOwnProperty(key)) {
+	// 					var obj = data[key];
+	// 					var Latlng = new google.maps.LatLng(obj.marker.lat, obj.marker.lng);
+	// 					var marker = new google.maps.Marker({ title: obj.title, animation: google.maps.Animation.DROP, position: Latlng,map: map, icon: 'img/marker.png' });
+	// 					map.markers.push(marker);
+	// 					var contentString = '<div class="map-marker"><h3>'+obj.title+'</h3><p>'+obj.marker.address+'</p></div>';
+	// 					var infowindow = new google.maps.InfoWindow({ content: obj.title });
+	// 					google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
+	// 						return function() {
+	// 							infowindow.setContent(content);
+	// 							infowindow.open(map,marker);
+	// 						};
+	// 					})(marker,contentString,infowindow));
+	// 					google.maps.event.addListener(map, 'click', (function(marker,content,infowindow) {
+	// 						return function() {
+	// 						infowindow.close(map,marker);
+	// 						};
+	// 					})(marker,contentString,infowindow));
+	// 					infowindow.setContent(contentString);
+	// 					infowindow.open(map,marker);
+	// 				}
+	// 			}
+	// 			map.set('styles', mapStyle);
+	// 			map.setOptions({'scrollwheel': false});
+	// 			var bounds = new google.maps.LatLngBounds();
+	// 			$.each(map.markers, function(i, marker){
+	// 				var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
+	// 				bounds.extend(latlng);
+	// 			});
+	// 			if( map.markers.length == 1 ) {
+	// 				map.setCenter( bounds.getCenter() );
+	// 			} else {
+	// 				map.fitBounds(bounds);
+	// 			}
+	// 		} else {
+	// 			$(this).remove();
+	// 		}
+	// 	});
+	// }
 
 	$.fn.positionPoints = function() {
 		$overlay = $(this);
@@ -130,7 +124,7 @@ $(document).ready(function(){
 			doScroll();
 		}).resize();
 
-		initializeMaps();
+		// initializeMaps();
 
 		$('.point').hover(function(e){
 			$point = $(this);
@@ -164,39 +158,21 @@ $(document).ready(function(){
 		});
 
 
-		$('a[href^="#"]').click(function(e){
-			e.preventDefault();
-			if ($(this).hasClass('cv')) {
-				$('body').addClass("hide");
-				var person = $(this).attr('href');
-				$(person).addClass("active");
-			} else {
-				var target = $(this).attr('href');
-				$('html, body').animate({
-					scrollTop: $(target).offset().top
-				}, 400);
-			}
-		});
+		// $('a[href^="#"]').click(function(e){
+		// 	e.preventDefault();
+		// 	if ($(this).hasClass('cv')) {
+		// 		$('body').addClass("hide");
+		// 		var person = $(this).attr('href');
+		// 		$(person).addClass("active");
+		// 	} else {
+		// 		var target = $(this).attr('href');
+		// 		$('html, body').animate({
+		// 			scrollTop: $(target).offset().top
+		// 		}, 400);
+		// 	}
+		// });
 
-		$(".close").click(function(){
-			$('body').removeClass("hide");
-			$('.cv-inner').removeClass("active");
-		});
-
-		function prevSection() {
-			var pos = $(document).scrollTop();
-			if (pos != 0) {
-				$($('.scrollTo').get().reverse()).each(function(i){
-					var elempos = $(this).position().top-pos;
-					if(elempos < 0) {
-						$('body, html').stop().animate({
-							scrollTop: elempos+pos-80
-						}, 400);
-						return false;
-					}
-				});
-			}
-		}
+		// 
 
 
 		function nextSection() {
@@ -256,14 +232,16 @@ $(document).ready(function(){
 		});
 
 
-		setTimeout(function(){
-			$(window).mousemove(function(e){
-				var X = e.pageX;
-				var Y = e.pageY;
-				var $elem = $('.point').closestToOffset({left:X , top:Y});
-				$('.point').removeClass("highlight visible");
-				$elem.addClass("highlight");
-			});
-		}, 1500);
+		// setTimeout(function(){
+		// 	$(window).mousemove(function(e){
+		// 		var X = e.pageX;
+		// 		var Y = e.pageY;
+		// 		var $elem = $('.point').closestToOffset({left:X , top:Y});
+		// 		$('.point').removeClass("highlight visible");
+		// 		$elem.addClass("highlight");
+		// 	});
+		// }, 1500);
+
 	});
+
 })(jQuery);
