fc0363e4ff3de7833a50b617f1623825fa10dbed
AbeeraKamran <abeera.k@gmail.com>
2015-07-20 11:20:13 +0100

    trying to make the menuHide js work

--- Tentative Collective Web/js/menuHide.js
+++ Tentative Collective Web/js/menuHide.js
@@ -1,11 +1,36 @@
-// $(function() {
-// 	$(".menu-icon").on('click', function() {
-// 	  $("nav ul").toggleClass('hide');
-//   	  $(".row").toggleClass('hide');
-//   	  var padding = $('#main').css('padding-top');
-//     var newPadding = (padding=='0px')?'650px':'0px';
-//     $('#main').css('padding-top',newPadding);
+$(function() {
+	
+
+	$('#nav-trigger').click(function(){
+		
+        $('#gradient').fadeOut('slow', function() {
+        	$('#nav').fadeIn('slow');
+        	});
+    });
+	// $(".nav-trigger").on('click', function() {
+		// $("nav").toggleClass('hide');
+		// $(".content").toggleClass('hide');
+		// $(".footer").toggleClass('hide');
+		// $(".overlay").toggleClass('hide');
 
 
-// 	});
-// });
+
+ //    if($("nav").hasClass('hide')){
+	// 	   $("nav").removeClass('hide').fadeIn( '1000' )
+	// 	}else{
+	// 	  $("nav").addClass('hide').fadeOut( '1000' )
+	// 	}
+
+	// 	if($(".content").hasClass('hide')){
+	// 	   $(".content").removeClass('hide').fadeIn( '1000' )
+	// 	}else{
+	// 	  $(".content").addClass('hide').fadeOut( '1000' )
+	// 	}
+	// 	if($(".footer").hasClass('hide')){
+	// 	   $(".footer").removeClass('hide').fadeIn( '1000' )
+	// 	}else{
+	// 	  $(".footer").addClass('hide').fadeOut( '1000' )
+	// 	}
+
+	// });
+});
