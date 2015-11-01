9762366d52a731e96bbf795c778994fb3a5c4889
AbeeraKamran <abeera.k@gmail.com>
2015-11-01 00:57:44 +0000

    writing media queries

--- exhausted geographies website/js/main.js
+++ exhausted geographies website/js/main.js
@@ -1,1 +1,39 @@
 
+// var box1 = $('.grid-overlay-1');
+
+// $('.item-1').hover(function () {
+  
+    
+//     box1.removeClass('hidden');
+//     setTimeout(function () {
+//       box1.removeClass('visuallyhidden');
+//     }, 20);
+
+
+    
+    
+//   });
+
+// var box2 = $('.grid-overlay-2');
+
+// $('.item-2').on('mouseover', function () {
+  
+//   if (box2.hasClass('hidden')) {
+    
+//     box2.removeClass('hidden');
+//     setTimeout(function () {
+//       box2.removeClass('visuallyhidden');
+//     }, 20);
+
+//   } else {
+    
+//     box2.addClass('visuallyhidden');
+    
+//     box2.one('transitionend', function(e) {
+
+//       box2.addClass('hidden');
+
+//     });
+    
+//   }
+// });
