e901dccb89efce18c0e4d0c2fb3ad46256562cb0
AbeeraKamran <abeera.k@gmail.com>
2016-12-20 14:39:59 +0000

    Update #1

--- OOMK-Internet Project/js/kushner.js
+++ OOMK-Internet Project/js/kushner.js
@@ -1,14 +1,13 @@
 //kushner pop-up//
 
-    function pageLoad()
+    // function pageLoad()
 
     {
-        alert('Respect the delicate ecology of your delusions');
+        alert('No sorrow here, 😢 freeze. Antartica, Antartica, oh boy oh boy 👁 at it, I..WOW, I mustve really ✂️ the tether, huh?Apparently...Thats 👍. I want to stay here ∞. Set up camp. Build things. Build a 🏙, an enormous 🏙 made up of frontier forts,dark wood and green roofs and high gates made of pointed logs and 🔥 burning on every street corner, I should build by a 💦. Where are the forests? No timber here. Too cold. ❄️, no 🌲. Oh details Im sick of details Ill plant them and 🌱 them, Ill live off caribou fat, Ill melt it over the 🔥 and drink it from long, curved goat-horn cups. Itll be great. I want to make a new 🌏 here. So that I never have to go home again. As long as it lasts. ❄️ has a way of melting...No. ∞. I can have anything I want here-maybe even 👩‍❤️‍👩, someone who has...desire for me. You, maybe. Its against the by-laws of the International Order of Travel Agents to get involved with clients. Rules are rules. Anyway, Im not the one you really want. There isnt anyone...maybe an Eskimo. Who could 🎣 for food. And help me build a nest for when the 👶 comes. There are no Eskimos in Antartica. And youre not really pregnant. You just made that up.Well all this is made up. So if the 🌨 feels cold Im pregnant. Right? Here, I can be pregnant. And I can have any kind of 👶🏾 I want. This is a retreat, a vacuum, its a virtue is that it lacks everything; deep-freeze for feelings. You can be numb and safe here, that is what you came for.');
+ 	}
+
+    {
+        alert('RESPECT THE DELICATE ECOLOGY OF YOUR DELUSIONS.');
     }
 
-    
-    {
-        alert('No sorrow here, tears freeze. Antartica, antartica, oh boy oh boy LOOK at it, I..Wow, I mustve really snapped the tether, huh?Apparently...Thats great. I want to stay here forever. Set up camp. Build things. Build a city, an enormous city made up of frontier forts,dark wood and green roofs and high gates made of pointed logs and bonfires burning on every street corner, I should build by a river. Where are the forests?No timber here. Too cold. Ice, no trees.Oh details Im sick of details Ill plant them and grow them, Ill live off caribou fat, Ill melt it over the bonfires and drink it from long, curved goat-horn cups. Itll be great. I want to make a new wolrd here. So that I never have to go home again. As long as it lasts. Ice has a way of melting...No. Forever. I can have anything I want here-maybe even companionship, someone who has...desire for me. You, maybe. Its against the by-laws of the International Order of Travel Agents to get involved with clients. Rules are rules. Anyway, Im not the one you really want. There isnt anyone...maybe an Eskimo. Who could ice-fish for food. And help me build a nest for when the baby comes. There are no Eskimos in Antartica. And youre not really pregnant. You just made that up.Well all this is made up. So if the snow feels cold Im pregant. Right? Here, I can be pregnant. And I can have any kind of baby I want. This is a retreat, a vacuum, its a virtue is that it lacks everything; deep-freeze for feelings. You can be numb and safe here, thats what you canme for.This is a retreat, a vacuum, it is a virtue is that it lacks everything; deep-freeze for feelings. You can be numb and safe here,that is what you came for.');
-     }
-
-    pageLoad(); 
+    // pageLoad(); 
